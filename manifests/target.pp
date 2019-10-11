dd84013d6b5025bb51c7b1eb50d242a51acba61c
Samuli Seppänen <samuli@openvpn.net>
2019-10-11 11:56:43 +0300

    Add bolt target support
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- manifests/target.pp
+++ manifests/target.pp
@@ -1,8 +1,21 @@
-# @summary A short summary of the purpose of this class
+# @summary setup bolt target node
 #
-# A description of what this class does
+# configure ssh and optionally sudo rules on a Bolt target node
 #
 # @example
 #   include bolt_ssh::target
-class bolt_ssh::target {
+#
+class bolt_ssh::target
+(
+  String $user,
+  String $ssh_public_key,
+  String $ssh_public_key_type
+)
+{
+  ssh_authorized_key { 'puppet-bolt.pub':
+    ensure => 'present',
+    user   => $user,
+    type   => $ssh_public_key_type,
+    key    => $ssh_public_key,
+  }
 }
